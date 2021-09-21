10962eea6d9306d453e378e8169fdf7a774ff115
WalukaUI <cwaluka@yahoo.com>
2021-09-21 15:49:28 -0700

    completed

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,13 @@
 export function manageFriends(state, action) {
-  // your code here
+          switch(action.type){
+            case "friends/add":
+              return {friends: [...friends, action.payload]}
+            case "friends/remove":
+              return {friends: filter((friend)=> friend.id !== action.payload)}  //find by id , payload directly points "id" here
+            default:
+                return state;
+          
+          }
+  }
+
 }
